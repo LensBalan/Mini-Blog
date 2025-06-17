71ef5be9f45b8df4d7e5471e1743d72963bfce69
Lens Balan <lensrambinho21@gmail.com>
2025-06-17 09:59:49 -0300

    primeiras alterações

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+//pages
+import Home from './pages/Home/Home.jsx';
+import About from './pages/About/About.jsx';
+import Navbar from './components/Navbar.jsx';
+import Footer from './components/Footer.jsx';
+
+
+function App() {
+
+  return (
+    <div className='App'>
+      <BrowserRouter>
+      <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/sobre' element={<About />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
