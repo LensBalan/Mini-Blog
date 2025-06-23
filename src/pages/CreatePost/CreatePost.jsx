43283d2230a8defba979b9d5d2563c3f3ae48aa5
Lens Balan <lensrambinho21@gmail.com>
2025-06-23 11:14:37 -0300

    new changes 23/06

--- src/pages/CreatePost/CreatePost.jsx
+++ src/pages/CreatePost/CreatePost.jsx
@@ -17,26 +17,41 @@ function CreatePost() {
 
   const {insertDocument, response} = useInsertDocument("posts");
 
+  const navigate = useNavigate();
+
   const handleSubmit = (e) => {
     e.preventDefault();
     setFormError("");
 
     //validar url da imagem
+    try {
+      new URL(image);
+
+    } catch (error) {
+      setFormError("A imagem precisa ser uma URL.");
+      
+    }
 
     //criar array de tags
+    const tagsArray = tags.split(",").map((tag) => tag.trim().toLowerCase());
 
     // checar todos os valores
+    if(!title || !image || !tags || !body){
+      setFormError("Por favor, preencha todos os campos.")
+    }
+    if(formError) return;
 
     insertDocument({
       title,
       image,
       body,
-      tags,
+      tagsArray,
       uid: user.uid,
       createdBy: user.displayName,
     })
 
     //redirect homepage
+    navigate("/");
 
   };
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,42 @@
 //Css
 import styles from './Home.module.css';
 
+//hooks
+import { useNavigate, Link } from 'react-router-dom';
+import { useState } from 'react';
+import {useFetchDocuments} from '../../hooks/useFetchDocuments';
+
+//components
+import PostDetail from '../../components/PostDetail';
+
 function Home() {
+
+  const [query, setQuery] = useState("");
+  const {documents:posts, loading} = useFetchDocuments("posts");
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <div>
-        <h1>Home</h1>
+    <div className={styles.home}>
+        <h1>Veja os nossos posts mais recentes</h1>
+        <form className={styles.search_form}>
+          <input type='text' placeholder='Ou busque por tags...' onChange={(e) => setQuery(e.target.value)} />
+          <button className='btn btn-dark'>Pesquisar</button>
+        </form>
+        <div>
+          {loading && <p>Carregando</p>}
+          {posts && posts.map((post) => (
+            <PostDetail  key={post.id} post={post} />
+          ))}
+          {posts && posts.length === 0 && (
+            <div className={styles.noposts}>
+              <p>Não foram encontrados posts</p>
+              <Link to='/posts/criar' className='btn'> Criar primeiro post</Link>
+            </div>
+          )}
+        </div>
     </div>
   )
 }
